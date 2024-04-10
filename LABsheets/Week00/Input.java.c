b72771d1d408b1fa685c0db5668f32315125c495
mahan zavari <154610980+mahanzavari@users.noreply.github.com>
2024-04-10 21:04:08 +0330

    Add files via upload

---
+++ LABsheets/Week00/Input.java.c
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        scanner.nextLine();
+        String c = scanner.nextLine();
+
+    }   }
---
+++ LABsheets/Week00/Mashinhesab.java.c
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class amalkard {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        double a=scanner.nextDouble();
+        double b= scanner.nextDouble();
+        scanner.nextLine();
+        String c=scanner.nextLine();
+
+        if(c.equals("+")){
+
+        }
+        if(c.equals("-")){
+        }
+        if(c.equals("*")){
+
+        }
+        if(c.equals("/")){
+        }
+
+
+
+
+    }
+}
